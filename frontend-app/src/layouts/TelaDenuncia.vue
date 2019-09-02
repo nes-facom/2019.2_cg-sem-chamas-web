b7fefee636eae98f55ee56a6bbbbed52ca08c696
Nicholas <nicholaspeterle@gmail.com>
2019-09-02 14:43:32 -0500

    Estrutura do App

---
+++ frontend-app/src/layouts/TelaDenuncia.vue
@@ -0,0 +1,16 @@
+<template>
+<head>
+<title>Tela de denuncia</title>
+ </head>
+  <div>
+<h1>CLIQUEIMADAS</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
