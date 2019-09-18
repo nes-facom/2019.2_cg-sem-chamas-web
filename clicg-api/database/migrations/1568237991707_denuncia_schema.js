4877cfc08778cb5c13be2df038e569090a266bb6
venino <julioveninoneto@hotmail.com>
2019-09-18 16:44:40 -0400

    Protocolo

--- clicg-api/database/migrations/1568237991707_denuncia_schema.js
+++ clicg-api/database/migrations/1568237991707_denuncia_schema.js
@@ -1,37 +1,34 @@
-"use strict";
+'use strict'
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use("Schema");
+const Schema = use('Schema')
+var d = Date.now();
 
 class DenunciaSchema extends Schema {
-  up() {
-    this.create("denuncias", table => {
-      table.increments();
+  up () {
+    this.create('denuncias', (table) => {
+      table.increments()
+      //table.bigInteger('idUserDenuncia').notNullable().unique()
+      table.bigInteger('idUserDenuncia').notNullable()//.unsigned().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
 
-      //table.bigInteger('idUserDenuncia').notNullable().unsigned().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
-      //table.bigInteger('idUserDenuncia').notNullable()
+      table.bigInteger('protocolo').notNullable().unique().defaultTo(d)
+      table.string('foto', 256).notNullable()
+      table.decimal('geo-lat', 9, 6)//.notNullable()
+      table.decimal('geo-lng', 9, 6)//.notNullable()
+      table.integer('intensidade').defaultTo(1)
+      table.string('observacao', 256)
+      table.date('data').notNullable()
+      table.string('ip', 16)
+      table.string('endereco', 256)//.notNullable()
 
-      table
-        .bigInteger("protocolo")
-        .notNullable()
-        .unique();
-      table.string("foto", 256).notNullable();
-      table.decimal("geo-lat", 9, 6);
-      table.decimal("geo-lng", 9, 6);
-      table.integer("intensidade").defaultTo(1);
-      table.string("observacao", 256);
-
-      table.date("data").notNullable();
-      table.string("ip", 16);
-      table.string("endereco", 256);
-
-      table.timestamps();
-    });
+      table.timestamps()
+    })
   }
 
-  down() {
-    this.drop("denuncias");
+
+  down () {
+    this.drop('denuncias')
   }
 }
 
-module.exports = DenunciaSchema;
+module.exports = DenunciaSchema
