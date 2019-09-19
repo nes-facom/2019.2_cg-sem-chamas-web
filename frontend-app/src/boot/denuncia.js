0d5be28706629991c36dfe93aefebb4f8abf1d2b
Lavínia <lavininhamelo@hotmail.com>
2019-09-18 20:38:50 -0400

    Axios Clean Code

---
+++ frontend-app/src/boot/denuncia.js
@@ -0,0 +1,23 @@
+import { http } from "./axios";
+
+export default {
+    salvar: denuncia => {
+        return http.post("denuncia", denuncia);
+    },
+
+    atualizar: denuncia => {
+        return http.put("denuncia", denuncia);
+    },
+
+    listar: () => {
+        return http.get("denuncias");
+    },
+
+    apagar: denuncia => {
+        return http.delete("denuncia", { data: denuncia });
+    },
+
+    exibir: denuncia => {
+        return http.get("denuncia", { data: denuncia });
+    }
+};
--- frontend-app/src/router/routes.js
+++ frontend-app/src/router/routes.js
@@ -1,26 +1,35 @@
-const routes = [
-{
-    path: '/',
-    component: () => import('layouts/MyLayout.vue'),
-    children: [
-      { path: 'camera', component: () => import('pages/Camera.vue') },
-      { path: 'gps', component: () => import('pages/Map.vue') },
-      { path: 'formulario', component: () => import('pages/FormularioDenuncia.vue') }
-    ] 
-  }, 
-  {
-    path: "/denuncia",
-    component: () => import("layouts/TelaDenuncia.vue")
-  }
+const routes = [{
+        path: "/",
+        component: () =>
+            import ("layouts/MyLayout.vue"),
+        children: [
+            { path: "camera", component: () =>
+                    import ("pages/Denuncias.vue") },
+            { path: "gps", component: () =>
+                    import ("pages/Map.vue") },
+            {
+                path: "formulario",
+                component: () =>
+                    import ("pages/FormularioDenuncia.vue")
+            },
+            { path: "denuncia", component: () =>
+                    import ("pages/Denuncias.vue") }
+        ]
+    },
+    {
+        path: "/denuncia",
+        component: () =>
+            import ("layouts/TelaDenuncia.vue")
+    }
 ];
-
 
 // Always leave this as last one
 if (process.env.MODE !== "ssr") {
-  routes.push({
-    path: "*",
-    component: () => import("pages/Error404.vue")
-  });
+    routes.push({
+        path: "*",
+        component: () =>
+            import ("pages/Error404.vue")
+    });
 }
 
 export default routes;
