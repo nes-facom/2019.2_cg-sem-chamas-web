bfc429fa668f5abbf486b4cc61a32d250a88adeb
Paulo <paulojreis243@gmail.com>
2019-10-14 16:30:33 -0400

    Mudanças login

--- frontend-web/src/pages/Login.vue
+++ frontend-web/src/pages/Login.vue
@@ -2,10 +2,10 @@
 <div class="container">
     <div class="form">
 
-        <h3>Login</h3>
+        <h3>Entrar</h3>
         <div class="img-centro"></div>
-        <q-input outlined label="username" :dense="dense" v-model="username" />
-       <q-input outlined label="password" :dense="dense" v-model="password" />
+        <q-input outlined label="username" :dense="dense"/>
+       <q-input outlined label="password" :dense="dense"/>
         <button type="button" onclick="login()">Login</button>
         </div>
         </div>
@@ -18,7 +18,6 @@
   display:flex;
   justify-content :center;
   align-items:center;
-  flex-direction: column;
 }
 
 .form {
@@ -30,24 +29,20 @@
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
 
-.form q-input {
+.form input {
   font-family: "Roboto", sans-serif;
-  outline: 0;
   background: #f2f2f2;
   width: 100%;
-  border: 0;
+  margin-top: 5%;
   margin: 15px;
   padding: 15px;
-  box-sizing: border-box;
   font-size: 14px;
 }
 .form button {
   font-family: "Roboto", sans-serif;
   text-transform: uppercase;
-  outline: 0;
   background:#F4853E;
   width: 100%;
-  border: 0;
   padding: 15px;
   color: #FFFFFF;
   font-size: 14px;
@@ -64,9 +59,8 @@
   background-color: blue;
   background: url("https://i.imgur.com/sV8gZBV.png");
   background-size: 220px auto;
-  width: 250px;
-  height: 250px;
-  border: 17px solid #f4853e;
+  width: 220px;
+  height: 200px;
   box-sizing: border-box;
 }
 
