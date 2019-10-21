fbcca1c43170a14f4579544c16b1d1f843c0562d
Paulo <paulojreis243@gmail.com>
2019-10-21 16:30:27 -0400

    Nova noticias

--- frontend-web/src/pages/Noticias.vue
+++ frontend-web/src/pages/Noticias.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="container" style="height: 100vh">
-    <div class="header">
+    <div class="titulo">
+      <h4 style="text-align: left; width: 85%; margin-left: 25px;">Nova publicação</h4>
+    </div>
+    <div class="header" style="height: 50%">
     <div class="image">
 
-      <div class="img"><q-icon name="add_photo_alternate" /></div>
+      <div class="img">
+        <a @click="mostrar()"><q-icon name="add_photo_alternate" class="text-grey" size="lg" style="padding: 30px;" /></a>
+
+        </div>
     </div>
     <div class="info">
       <div class="titulo">
         <q-input v-model="text" type="text" label="Título da Notícia" />
       </div>
       <div class="descricao">
-        <q-input v-model="text" type="text" label="Descrição da Notícia" />
+        <q-input v-model="text" type="textarea" label="Descrição da Notícia" />
+
       </div>
     </div>
     </div>
     <div class="bottom">
-      <q-editor v-model="editor" min-height="5rem" flat/>
-      <q-btn color="primary" icon="check" label="OK" @click="onClick" />
+      <template>
+    <q-editor v-model="editor" class="editor" label="Conteúdo da Notícia"/>
+
+
+</template>
+
+      <q-btn class="btn-enviar" color="primary" icon="check" label="Postar" @click="onClick" />
     </div>
   </div>
 </template>
 
 <style lang="stylus" scoped>
 .container{
+  margin: 0 auto;
+  width: 85%;
   display:flex;
   flex-direction:column;
 
@@ -36,6 +50,7 @@
 display: flex;
 flex-direction:column;
 width: 50%;
+margin: 25px;
 
 }
 
@@ -46,13 +61,34 @@ width: 50%;
 }
 
 .header .image{
+  display: flex;
   justify-content: center;
   align-items: center;
   width: 50%;
+  background: #eee;
+  margin: 25px;
+
 }
+
+
 
 .bottom{
   display:flex;
   flex-direction:column;
+  justify-content: center;
+  align-items: center;
+}
+
+.editor{
+  min-height:10%;
+  margin-top:15px;
+  margin-bottom: 25px;
+
+  width:97%;
+}
+
+.btn-enviar{
+  height: 50px;
+  width: 10%;
 }
 </style>
