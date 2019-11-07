c3e3574a900ee32fd3de353c24695806d0c4ca50
NicholasPeterle <nicholaspeterle@gmail.com>
2019-11-07 11:12:56 -0400

    CSS SIGNUP

--- frontend-app/src/pages/SignUp.vue
+++ frontend-app/src/pages/SignUp.vue
@@ -3,25 +3,24 @@
   width: 100vw;">
     <q-icon name="arrow_back_ios" class="voltar text-white" @click="voltar()" />
 
-    <h4>CG sem chamas</h4>
-
+    <div class="topo">
+      <div class="img-centro"></div>
+    </div>
     <div class="SignUp">
       <h6>Cadastrar-se</h6>
 
       <div class="inputs">
-        <q-input outlined v-model="text" label="Nome completo">
-          <template v-slot:prepend>
-            <q-icon name="person_add" />
-          </template>
-        </q-input>
-        <q-input outlined v-model="email" label="Email">
-          <template v-slot:prepend>
-            <q-icon name="mail" />
-          </template>
-        </q-input>
+        <q-input class="qinput" label="Nome completo" v-model="text" />
 
-        <q-input outlined :type="isPwd ? 'password' : 'text'" label="Senha" v-model="password">
-          <template v-slot:prepend>
+        <q-input class="qinput" label="E-mail" v-model="email" />
+
+        <q-input
+          class="qinput"
+          :type="isPwd ? 'password' : 'text'"
+          label="Senha"
+          v-model="password"
+        >
+          <template v-slot:append>
             <q-icon
               :name="isPwd ? 'visibility_off' : 'visibility'"
               class="cursor-pointer"
@@ -29,14 +28,13 @@
             />
           </template>
         </q-input>
-
         <q-input
-          outlined
+          class="qinput"
           :type="isPwd ? 'password' : 'text'"
-          label="Confirmar senha"
+          label="Senha"
           v-model="password"
         >
-          <template v-slot:prepend>
+          <template v-slot:append>
             <q-icon
               :name="isPwd ? 'visibility_off' : 'visibility'"
               class="cursor-pointer"
@@ -45,34 +43,10 @@
           </template>
         </q-input>
       </div>
-
-      <!-- <div class="btn-registrar">
-        <q-btn label="REGISTRAR" color="primary" @click="small = true" />
-        <q-dialog v-model="small">
-          <q-card class style="width: 300px">
-            <q-card-section>
-              <div class="closePop">
-                <q-btn flat label="X" v-close-popup to="/TelaDenuncia" />
-              </div>
-              <q-card-section>
-                <q-icon
-                  class="iconDenunciaRegistrada"
-                  name="fas fa-check-circle"
-                  style="font-size: 50px;"
-                ></q-icon>
-                <div class="popupUserRegistrado">Sucesso!</div>
-              </q-card-section>
-            </q-card-section>
-            <q-card-section>Cadastro realizado com sucesso.</q-card-section>
-
-            <q-card-actions align="right" class="bg-white text-teal">
-              <q-btn flat label="OK" v-close-popup to="/login" />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>-->
       <div class="btn-registrar">
-        <q-btn label="Registrar" color="primary" @click="small = true" />
+        <q-btn outline color="primary" label="Registrar" @click="small = true" />
+
+        <!-- <q-btn label="Registrar" color="primary" @click="small = true" /> -->
         <q-dialog v-model="small">
           <q-card class="popRegistrar">
             <div class="denunciaRegistradaTopo" style="width: 300px">
@@ -153,7 +127,8 @@ export default {
   background-color: #ffffff;
   width: 88%;
   border-radius: 2.2%;
-  margin-top: 10%;
+  margin-top: 0px;
+  margin-bottom: 20px;
 }
 h4 {
   font-family: "Robotos lab";
@@ -163,8 +138,10 @@ h4 {
   margin-bottom: 5%;
 }
 .inputs .q-input {
-  margin-top: 1%;
-  margin-bottom: 5%;
+  padding-top: 1px;
+  padding-bottom: 1px;
+  margin-top: 1px;
+  margin-bottom: 1px;
   width: 250px;
 }
 
@@ -175,6 +152,10 @@ q-card_section {
   color: #f4853e;
   margin-top: 5%;
 }
+/* .qinput {
+  padding-top: 1px;
+  padding-bottom: 1px;
+} */
 button {
   border-color: #f4853e;
   margin-top: 3%;
@@ -183,13 +164,13 @@ button {
   width: 250px;
 }
 h6 {
-  color: #737373;
+  color: black;
   margin: 4%;
 }
 p {
-  margin-top: 1%;
+  margin-top: 15px;
   font-size: 13px;
-  margin-bottom: 5%;
+  margin-bottom: 20px;
 }
 
 .btn-registrar button {
@@ -248,5 +229,20 @@ p {
   text-align: center;
   padding: 1%;
   color: #737373;
+}
+.img-centro {
+  background-color: blue;
+  background: url("https://i.imgur.com/h20GKNd.png");
+  background-size: 140px auto;
+  width: 140px;
+  height: 140px;
+  /* border: 17px solid #f4853e; */
+  box-sizing: border-box;
+  margin: 0px;
+  margin-bottom: 10px;
+}
+.topo {
+  margin-top: 50px;
+  margin-bottom: 0px;
 }
 </style>
