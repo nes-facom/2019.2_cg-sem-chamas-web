8df79fa6adbb087ec638b15fde8be0742ebf9aa5
Lavínia <lavininhamelo@hotmail.com>
2019-11-16 01:28:47 -0400

    Adição do Botão de Logout

--- frontend-app/src/layouts/Principal.vue
+++ frontend-app/src/layouts/Principal.vue
@@ -2,12 +2,19 @@
   <q-layout view="hHh lpR fFf">
     <q-header class="bg-grey-8 text-white">
       <q-toolbar>
-        <q-btn flat round dense icon="lavinia" class="q-mr-sm" />
+        <q-btn flat round dense icon="null" class="q-mr-sm" />
 
         <q-toolbar-title text-center style="font-size: 16px; margin-left: 60px"
           >CG SEM CHAMAS</q-toolbar-title
         >
-        <q-btn flat round dense icon="more_vert" class="q-mr-sm" />
+        <q-btn
+          flat
+          round
+          dense
+          icon="exit_to_app"
+          class="q-mr-sm"
+          @click="logout()"
+        />
       </q-toolbar>
     </q-header>
 
@@ -22,6 +29,9 @@
 </template>
 
 <script>
+import User from '../boot/login';
+import { store } from '../store/index';
+
 export default {
   data() {
     return {
@@ -29,6 +39,23 @@ export default {
     };
   },
   methods: {
+    logout() {
+      const vm = this;
+      let token = localStorage.getItem('userToken');
+      User.deslogar(token)
+        .then(() => {
+          localStorage.removeItem('userToken');
+          vm.$store.commit('Session/updateNome', null);
+          vm.$store.commit('Session/updateEmail', null);
+          vm.$store.commit('Session/updateTelefone', null);
+          vm.$store.commit('Session/updateId', null);
+          vm.$router.push('/login');
+        })
+        .catch(() => {
+          localStorage.removeItem('userToken');
+          vm.$router.push('/login');
+        });
+    },
     voltar() {
       window.history.back();
     }
