7a4fc3be40518c4eac12a94bc45de0ba08e9bf50
lavininhamelo <lavininhamelo@hotmail.com>
2019-11-28 20:48:03 -0400

    Transferência GitLab FACOM

--- frontend-app/src/router/auth.js
+++
@@ -1,8 +0,0 @@
-import Session from '../store/module/Session';
-export default function authUser(to, from, next) {
-  if (!localStorage.getItem('userToken') || Session.state.id == null) {
-    next('/login');
-  } else {
-    next();
-  }
-}
--- sonar-project.js
+++
@@ -1,8 +0,0 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://localhost:9000',
-       options : {
-       'sonar.sources': '.',
-       'sonar.inclusions' : 'frontend-app/src/**, frontend-web/src/**, back-end/app/**' // Entry point of your code
-       }
-     }, () => {});
